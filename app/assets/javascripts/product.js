46eed44679e9f2e6291b767cbaa076c72b2e1e33
kanekotetsuya <tetsuyan78@i.softbank.jp>
2017-07-17 12:56:49 +0900

    fix JavaScript

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,31 +1,41 @@
-$(function(){
+$(window).bind("load", function(){
 
-  function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
+  // URLにhogehogeが含まれていたら実行
+  if(document.URL.match(/\/products\/\d+/)) {
 
-    for (var i = 0, f; f = files[i]; i++) {
-      if (!f.type.match('image.*')) {
-        continue;
+    $(function(){
+
+      function handleFileSelect(evt) {
+        var files = evt.target.files; // FileList object
+
+        for (var i = 0, f; f = files[i]; i++) {
+          if (!f.type.match('image.*')) {
+            continue;
+          }
+
+          var reader = new FileReader();
+
+          reader.onload = (function(theFile) {
+            return function(e) {
+              $('#current-image').remove();
+              var span = ['<img id="current-image" width="250" height="230" src="', e.target.result,
+                                '" title="', escape(theFile.name), '"/>'].join('');
+              $('#js-show-image-field').append(span);
+            };
+          })(f);
+
+          reader.readAsDataURL(f);
+        }
       }
 
-      var reader = new FileReader();
+      document.getElementById('file').addEventListener('change', handleFileSelect, false);
 
-      reader.onload = (function(theFile) {
-        return function(e) {
-          $('#current-image').remove();
-          var span = ['<img id="current-image" width="250" height="230" src="', e.target.result,
-                            '" title="', escape(theFile.name), '"/>'].join('');
-          $('#js-show-image-field').append(span);
-        };
-      })(f);
 
-      reader.readAsDataURL(f);
-    }
+    });
+
+
+
   }
 
-  document.getElementById('file').addEventListener('change', handleFileSelect, false);
-
-
 });
-
 
